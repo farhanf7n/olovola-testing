bb5ee338009baa37de1cb4ac60b4eedea68dcc41
Farhan Ahmed <farhanf7n@gmail.com>
2024-05-10 06:03:16 +0500

    Removed random svg

--- src/components/FAQ.jsx
+++ src/components/FAQ.jsx
@@ -24,36 +24,6 @@ const Accordion = () => {
           <img src={faqImage} className="w-max-[524px] h-max-[370px]" alt="" />
         </div>
       </div>
-
-      <div className="absolute bottom-0 right-0 z-[-1]">
-        <svg
-          width="1440"
-          height="886"
-          viewBox="0 0 1440 886"
-          fill="none"
-          xmlns="http://www.w3.org/2000/svg"
-        >
-          <path
-            opacity="0.5"
-            d="M193.307 -273.321L1480.87 1014.24L1121.85 1373.26C1121.85 1373.26 731.745 983.231 478.513 729.927C225.976 477.317 -165.714 85.6993 -165.714 85.6993L193.307 -273.321Z"
-            fill="url(#paint0_linear)"
-          />
-          <defs>
-            <linearGradient
-              id="paint0_linear"
-              x1="1308.65"
-              y1="1142.58"
-              x2="602.827"
-              y2="-418.681"
-              gradientUnits="userSpaceOnUse"
-            >
-              <stop stopColor="#3056D3" stopOpacity="0.36" />
-              <stop offset="1" stopColor="#F5F2FD" stopOpacity="0" />
-              <stop offset="1" stopColor="#F5F2FD" stopOpacity="0.096144" />
-            </linearGradient>
-          </defs>
-        </svg>
-      </div>
     </section>
   );
 };
