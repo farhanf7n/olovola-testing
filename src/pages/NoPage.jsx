fc29290f0413b1248f21a5027e8e2129500802d3
Farhan Ahmed <Inferno@Farhans-MacBook-Pro.local>
2024-05-19 20:40:51 +0500

    UD from 19may-3

--- src/pages/NoPage.jsx
+++ src/pages/NoPage.jsx
@@ -1,7 +1,42 @@
 import React from "react";
+import { Link } from "react-router-dom";
+
+import Error404 from "../assets/images/Error404.gif";
 
 function NoPage() {
-  return <div>NoPage</div>;
+  return (
+    <div>
+      <div
+        className="flex flex-col items-center justify-center h-100"
+        style={{ height: `calc(100vh - 98px)` }}
+      >
+        <div className="flex flex-col justify-center items-center">
+          <h1 className="flex flex-col text-[36px] font-ManropeEB sm:text-[80px] text-primary  leading-[44px] sm:leading-[73px] pb-4 tracking-tight">
+            404
+          </h1>
+
+          <img src={Error404} className="h-[400px]" alt="" />
+
+          <div className="flex flex-col justify-center items-center gap-2">
+            <div>
+              <h3 className="flex flex-col text-3xl font-ManropeSB items-center">
+                Look like you're lost
+              </h3>
+              <p className="text-center px-[16px] text-lg flex flex-col font-ManropeRegular">
+                the page you are looking for not avaible!
+              </p>
+            </div>
+            <Link
+              to="/"
+              className="whitespace-nowrap text-[14px] bg-primary hover:bg-[#080C09] hover:text-[#FDD700] py-[10px] px-[20px] rounded-[8px] font-ManropeMedium transition-all duration-300 ease-in-out"
+            >
+              Go to Home
+            </Link>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default NoPage;
--- src/pages/Services.jsx
+++ src/pages/Services.jsx
@@ -1,7 +1,41 @@
 import React from "react";
 
 function Services() {
-  return <div>Services</div>;
+  return (
+    <div>
+      <div
+        className="flex flex-col items-center justify-center h-100"
+        style={{ height: `calc(100vh - 98px)` }}
+      >
+        <div className="text-5xl font-semibold mb-3">
+          This Page Is Under Construction
+        </div>
+        <div className="opacity-50 mb-8">Will be up and running soon.</div>
+        <div className="animate-bounce">
+          <svg
+            className="animate-spin  -ml-1 mr-3 h-8 w-8 text-black"
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+          >
+            <circle
+              className="opacity-25"
+              stroke="currentColor"
+              strokeWidth="4"
+              cx="12"
+              cy="12"
+              r="10"
+            ></circle>
+            <path
+              className="opacity-75"
+              fill="currentColor"
+              d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+            ></path>
+          </svg>
+        </div>
+      </div>
+    </div>
+  );
 }
 
 export default Services;
